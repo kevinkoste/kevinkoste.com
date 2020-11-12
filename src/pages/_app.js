278d0d57e66d819a1745d4ad5857182054012953
Kevin Koste <kevin.koste@yale.edu>
2020-11-12 01:04:22 -0800

    readme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,6 @@
-// import { useEffect } from 'react'
-// import { useRouter } from 'next/router'
-// import * as Fathom from 'fathom-client'
-
 import './_app.css'
 
 const App = ({ Component, pageProps }) => {
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   // Initialize Fathom when the app loads
-  //   Fathom.load('ECJIK', {
-  //     // url: '/tracker.js',
-  //     includedDomains: ['kevinkoste.com'],
-  //   })
-
-  //   console.log('in fathom loader, window.fathom is:', window.fathom)
-
-  //   const onRouteChangeComplete = () => {
-  //     Fathom.trackPageview()
-  //   }
-  //   // Record a pageview when route changes
-  //   router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-  //   // Unassign event listener
-  //   return () => {
-  //     router.events.off('routeChangeComplete', onRouteChangeComplete)
-  //   }
-  // }, [router])
-
   return <Component {...pageProps} />
 }
 
