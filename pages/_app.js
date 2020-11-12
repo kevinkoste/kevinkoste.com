5a31d1da06f7181a0f04977907cf1b350beb1d27
Kevin Koste <kevin.koste@yale.edu>
2020-11-11 20:33:50 -0800

    adding fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,30 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
 import './_app.css'
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('ECJIK', {
+      includedDomains: ['kevinkoste.com'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
